9842c1a76eee160901dfa5848fe9579206f16eef
rodolfoAcabrera <rodo555666@gmail.com>
2023-01-17 02:54:24 -0300

    EWDS

---
+++ src/partes/YoPersonal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './yoPersonal.css'
+
+const YoPersonal = () => {
+
+
+
+
+
+  return (
+    <><div></div>
+    
+    <img className='foto_mia' src="yo.jpg" alt="fotografia" />
+    <h1 className='nombre'> Rodolfo A. Cabrera</h1>
+    
+    </>
+  )
+}
+
+export default YoPersonal
