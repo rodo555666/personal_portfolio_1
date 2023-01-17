1676e41d761b38fe324f46c5772c7c5615288073
rodolfoAcabrera <rodo555666@gmail.com>
2023-01-17 20:03:52 -0300

    WEDSFDS

--- src/partes/YoPersonal.jsx
+++ src/partes/YoPersonal.jsx
@@ -8,10 +8,15 @@ const YoPersonal = () => {
 
 
   return (
-    <><div></div>
-    
-    <img className='foto_mia' src="yo.jpg" alt="fotografia" />
+    <><div> </div>
+    <div className='con'><img className='foto_mia' src="yo.jpg" alt="fotografia" />
+   <div className='text'> 
+    <h2>Hola soy</h2>
     <h1 className='nombre'> Rodolfo A. Cabrera</h1>
+    <h2 className='front'>Frontend Development</h2>
+    </div>
+    </div>
+  
     
     </>
   )
